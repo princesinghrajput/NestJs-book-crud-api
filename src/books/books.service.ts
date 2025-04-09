b26038b87cbefba569662c06c5e130d1e9fa704d
princesinghrajput <psr8084@gmail.com>
2025-04-09 23:09:36 +0530

    feat: setup basic services

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+interface Book {
+    id: number;
+    title: string;
+    author: string;
+    publishedYear: number;
+}
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+    private books: Book[] = [];
+    private idCounter = 1;
+
+
+    create(book: CreateBookDto) {
+        const newBook  = {id: this.idCounter++, ...book};
+        this.books.push(newBook);
+        return newBook;
+    }
+
+    findAll() {
+        return this.books;
+    }
+    
+
+    findOne(id: number) {
+        return this.books.find(book => book.id === id);
+    }
+
+    update(id: number, updateBookDto: UpdateBookDto) {
+        const bookIndex = this.books.findIndex(book => book.id === id);
+        if (bookIndex === -1) {
+            throw new NotFoundException('Book not found');
+        }
+        this.books[bookIndex] = {...this.books[bookIndex], ...updateBookDto};
+        return this.books[bookIndex];
+    }
+
+    remove(id: number) {
+        const bookIndex = this.books.findIndex(book => book.id === id);
+        if (bookIndex === -1) {
+            throw new NotFoundException('Book not found');
+        }
+        this.books.splice(bookIndex, 1);
+    }
+}
